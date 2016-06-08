95cb06d684cc3780e3afda39a7559a4207d93d34
Stephen Pinkerton <stephen@pinkerton.io>
2016-06-08 12:04:19 -0700

    Initial commit

---
+++ tests/mapreduce_tests.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+    // include your C header files here
+}
+
+
+TEST(TestTest, Add) {
+    EXPECT_EQ(1+1, 2);
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
